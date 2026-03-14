224d4d143b1eacd359bcba596e283ef87c5deb22
thecode-master-sy <thecodemasster@gmail.com>
2026-03-14 06:41:17 +0100

    feat: featured product section

---
+++ app/_sections/featured-products.tsx
@@ -0,0 +1,10 @@
+export default function FeaturedProducts() {
+  return (
+    <div className="featured-products py-12 relative z-2 bg-background px-4">
+      <h2 className="text-h2 uppercase -tracking-[0.03em]">
+        Featured Products
+      </h2>
+      <div className="product-grid">{/* Product Cards */}</div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "./_sections/hero-section";
 import DiscoverDiffusers from "./_sections/discover-diffuser";
 import AboutSection from "./_sections/about-us";
+import FeaturedProducts from "./_sections/featured-products";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <HeroSection />
       <DiscoverDiffusers />
       <AboutSection />
+      <FeaturedProducts />
     </main>
   );
 }
